bb104698c710658c7b97a8e1f85073041a99f1b2
Unknown <sam@malpass.me.uk>
2020-03-12 07:48:50 +0000

    Create project files

---
+++ OpenCV-Object-Recognition/OpenCV-Object-Recognition.cpp
@@ -0,0 +1,12 @@
+﻿// OpenCV-Object-Recognition.cpp : Defines the entry point for the application.
+//
+
+#include "OpenCV-Object-Recognition.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
