b3ece195267bc8830339920f5f5a1d523b5ec632
Unknown <sam@malpass.me.uk>
2020-03-12 08:23:27 +0000

    Declare the Preprocessor class

--- OpenCV-Object-Recognition/Header Files/Preprocessor.h
+++ OpenCV-Object-Recognition/Header Files/Preprocessor.h
@@ -1,2 +1,8 @@
 #pragma once
 #include "Structues.h"
+
+/* Preprocessor class handles the preprocessing of frames so they can be used in conjunction with the SVM */
+class Preprocessor {
+public:
+	
+};
