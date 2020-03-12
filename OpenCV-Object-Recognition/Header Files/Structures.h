431dd04af6248105ec79010d808ba39f7a6df583
Unknown <sam@malpass.me.uk>
2020-03-12 08:01:33 +0000

    Add Processed_Image struct

--- OpenCV-Object-Recognition/Header Files/Structures.h
+++ OpenCV-Object-Recognition/Header Files/Structures.h
@@ -10,3 +10,13 @@ struct Image {
 	// The Depth component of the frame
 	Mat Depth_Component;
 };
+
+/* Structure for storing the processed frame data */
+struct Processed_Image {
+	// A copy of the original image
+	Image original_image;
+	// The Masked RGB component of the frame
+	Mat RGB_Masked;
+	// A vector of points that make up the contour of the image
+	vector<Point> outline;
+};
