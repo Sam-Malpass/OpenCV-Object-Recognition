0d40fd289e42340e94248ed4fae1ea520c9cce39
Unknown <sam@malpass.me.uk>
2020-03-12 09:51:28 +0000

    Define all testing functions

--- OpenCV-Object-Recognition/Testing Code/Tests.h
+++ OpenCV-Object-Recognition/Testing Code/Tests.h
@@ -12,17 +12,18 @@ using namespace std;
 
 /* Tester class has access to argument generating functions */
 class Tester {
-public:
 	// Generates command line arguments for training on my laptop
-	string* train_laptop();
+	string* train_laptop(); //1
 	// Generates command line arguments for testing on my laptop
-	string* test_laptop();
+	string* test_laptop(); //2
 	// Generates command line arguments for training on my desktop
-	string* train_desktop();
+	string* train_desktop(); //3
 	// Generates command line arguments for testing on my desktop
-	string* test_desktop();
+	string* test_desktop(); //4
 	// Generates command line arguments for creating a confusion matrix on my laptop
-	string* confusion_laptop();
+	string* confusion_laptop(); //5
 	// Generates command line arguments for creating a confusion matrix on my desktop
-	string* confusion_desktop();
+	string* confusion_desktop(); //6
+public:
+	string* test(int test);
 };
