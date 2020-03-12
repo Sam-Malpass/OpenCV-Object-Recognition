eae5fc88e69b3004cf120e1d6e162ba66784e2b7
Unknown <sam@malpass.me.uk>
2020-03-12 09:08:53 +0000

    Define balanceData function

--- OpenCV-Object-Recognition/Source Files/Classifiers.cpp
+++ OpenCV-Object-Recognition/Source Files/Classifiers.cpp
@@ -1,8 +1,50 @@
 #include "Classifiers.h"
 
+/* Finds the class with the smallest number of frames, then ensures that all the classes have that number of frames */
 vector<vector<Mat>> SVM::balanceData(vector<vector<Mat>> inputs)
 {
-	return vector<vector<Mat>>();
+	// Creates a vector to store the balanced training data
+	vector<vector<Mat>> balanced;
+	// Declares the smallest sized class to be max int
+	int smallest = INT_MAX;
+
+	// For all class samples
+	for (vector<Mat> object : inputs) {
+		// Check if the sample size is less than the current smallest
+		if (sizeof(object) < INT_MAX) {
+			// Update the smallest sample size
+			smallest = sizeof(object);
+		}
+	}
+
+	// For all class samples
+	for (vector<Mat> object : inputs) {
+		// Declare a new vector to hold the balanced sample
+		vector<Mat> balanced_object;
+		// Declare a vector to track used entries (Sample without replacement)
+		vector<int> used;
+		// For the number of frames we want in the sample
+		for (int i = 0; i < smallest; i++) {
+			// Declare an int
+			int rnd;
+			do {
+				// Find a random int in the range 0 to sample size
+				rnd = rand() % smallest;
+				// Check whether that element is in the list
+			} while (find(used.begin(), used.end(), rnd) != used.end());
+			balanced_object.push_back(object[rnd]);
+			// Add the index to the list of used indices
+			used.push_back(rnd);
+		}
+		// Push back the balanced class sample into the balanced set
+		balanced.push_back(balanced_object);
+		// Clear the object sample
+		balanced_object.clear();
+		// Clear the index tracker
+		used.clear();
+	}
+	// Return the balanced data
+	return balanced;
 }
 
 ShuffledData SVM::shuffleRows(Mat data, vector<int> labels)
