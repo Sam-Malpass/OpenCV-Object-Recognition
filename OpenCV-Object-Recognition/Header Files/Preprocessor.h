950d1a170e5d89d7c52b9d4725ad57d4b18623d2
Unknown <sam@malpass.me.uk>
2020-03-12 08:24:54 +0000

    Declare the functions in the Preprocessor class

--- OpenCV-Object-Recognition/Header Files/Preprocessor.h
+++ OpenCV-Object-Recognition/Header Files/Preprocessor.h
@@ -4,5 +4,14 @@
 /* Preprocessor class handles the preprocessing of frames so they can be used in conjunction with the SVM */
 class Preprocessor {
 public:
-	
+	// Constructor
+	Preprocessor();
+	// Converts the RGB component into grayscale
+	Mat toGrayscale(Mat rgb);
+	// Translates the Depth component to be more in-line with the RGB component
+	Mat translateDepth(Mat depth);
+	// Thresholds the image
+	Mat thresholdImage(Mat depth, Mat grayscale);
+	// Find the contours in the image - assume largest area is the object
+	Contour_Data findContours(Mat thresholded_image);
 };
