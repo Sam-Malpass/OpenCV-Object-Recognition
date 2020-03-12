1975d0d3399461ce3c439343b6970895fcddf7f4
Unknown <sam@malpass.me.uk>
2020-03-12 08:28:45 +0000

    Define the toGrayscale function

--- OpenCV-Object-Recognition/Source Files/Preprocessor.cpp
+++ OpenCV-Object-Recognition/Source Files/Preprocessor.cpp
@@ -1,1 +1,35 @@
 #include "Preprocessor.h"
+
+/* Constructor with no arguments */
+Preprocessor::Preprocessor()
+{
+	// Empty, just needed to create an object
+}
+
+/* Takes an RGB image matrix and coverts it into a singular grayscale matrix */
+Mat Preprocessor::toGrayscale(Mat rgb)
+{
+	// Create the matrix to hold the grayscale
+	Mat grayscale;
+	// use the RGB data to create the grayscale image
+	cvtColor(rgb, grayscale, COLOR_RGB2GRAY);
+	// Threshold the grayscale image
+	threshold(grayscale, grayscale, 190, 255, THRESH_BINARY);
+	// Return the now gray image
+	return grayscale;
+}
+
+Mat Preprocessor::translateDepth(Mat depth)
+{
+	return Mat();
+}
+
+Mat Preprocessor::thresholdImage(Mat depth, Mat grayscale)
+{
+	return Mat();
+}
+
+Contour_Data Preprocessor::findContours(Mat thresholded_image)
+{
+	return Contour_Data();
+}
